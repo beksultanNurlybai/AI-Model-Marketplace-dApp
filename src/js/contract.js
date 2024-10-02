4d0cf9b5e2fdf0822dcdb9e7bcfea3e54983255d
simpoyash <simpoyash@gmail.com>
2024-10-02 19:52:01 +0500

    init

---
+++ src/js/contract.js
@@ -0,0 +1,36 @@
+const contractAddress = '0x24A967c408f40753DA7b5b9026aE1a3f8d137299';
+let web3;
+let contract;
+
+async function connectWeb3() {
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            console.log("Connected to Ethereum via MetaMask");
+        } catch (error) {
+            console.error("User denied account access");
+        }
+    } else {
+        alert('Non-Ethereum browser detected. Please install MetaMask!');
+        console.log('Non-Ethereum browser detected. Please install MetaMask!');
+    }
+}
+
+async function loadContract() {
+    const response = await fetch('../build/contracts/AIModelMarketplace.json');
+    const contractData = await response.json();
+    return new web3.eth.Contract(contractData.abi, contractAddress);
+}
+
+async function getContract() {
+    if (!web3) {
+        await connectWeb3();
+    }
+    if (!contract) {
+        contract = await loadContract();
+    }
+    return contract;
+}
+
+connectWeb3();
---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+async function loadAvailableModels() {
+    const contract = await getContract();
+    const totalModels = await contract.methods.totalModels().call();
+    const modelList = document.getElementById('model-list');
+    modelList.innerHTML = '';
+
+    for (let i = 0; i < totalModels; i++) {
+        const model = await contract.methods.getModelDetails(i).call();
+        const modelHTML = `
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title">${model.name}</h5>
+                        <p class="card-text">Price: ${web3.utils.fromWei(model.price, 'ether')} Ether</p>
+                        <p class="card-text">Creator: ${model.creator}</p>
+                        <div class="d-flex justify-content-between">
+                            <a href="detail.html?id=${i}" class="btn btn-info">View</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        modelList.innerHTML += modelHTML;
+    }
+}
+
+loadAvailableModels();
